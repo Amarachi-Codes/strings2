2d2183ef25b36ebb1274b5f1fcc1e58134204d72
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-23 17:10:48 +0100

    arraycount solved

--- arraycount9.js
+++ arraycount9.js
@@ -6,3 +6,12 @@ Examples
 arrayCount9([1,2,9]) → 1
 arrayCount9([1,9,9]) → 2
 arrayCount9([1,9,9,3,9]) → 3
+
+function arrayCount9(nums){
+    numsCount = 0
+    for(let i = 0; i < nums.length; i++){
+    if(nums[i] === 9){
+     numsCount++
+    }
+       }return numsCount
+  }
