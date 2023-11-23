6ab65447f714ab91ef6cb75fc5406ee67bd865ef
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-23 17:29:17 +0100

    updated

--- arrayfront9.js
+++ arrayfront9.js
@@ -6,3 +6,11 @@
 // arrayFront9([1,2,9,3,4]) → true
 // arrayFront9([1,2,3,4,9]) → false
 // arrayFront9([1,2,3,4,5]) → false
+
+function arrayFront9(arr){
+    if(arr[0] === 9 || arr[1] === 9 || arr[2] === 9 || arr[3] === 9){
+    return true
+    
+    }return false
+    
+  }
